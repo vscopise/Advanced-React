2f5f70b78894d4633740f8b0b556e251f3ff3665
vscopise <vscopise@gmail.com>
2022-06-21 14:51:15 -0300

    chapter10

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,9 @@
+import Page from '../components/Page';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <p>Hello index page</p>
+    </div>
+  );
+}
---
+++ sick-fits/frontend/pages/products.js
@@ -0,0 +1,7 @@
+export default function ProductPage() {
+  return (
+    <div>
+      <p>Hello product page</p>
+    </div>
+  );
+}
